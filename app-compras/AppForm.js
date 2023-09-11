baa77c730308104daf0ae0d746715bc91e1cab5a
Aula AndroidStudio <aula.android@fatectq.local>
2023-09-11 10:47:25 -0300

    atualização

--- app-compras/AppForm.js
+++ app-compras/AppForm.js
@@ -1,30 +1,46 @@
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Database from './Database';
 
 
-export default function AppForm() {
+export default function AppForm({ navigation }) {
+
+  const [descricao, setDescricao] = useState(''); 
+  const [quantidade, setQuantidade] = useState('');
+  
+  function handleDescriptionChange(descricao){ setDescricao(descricao); } 
+  function handleQuantityChange(quantidade){ setQuantidade(quantidade); }
+  async function handleButtonPress(){ 
+    const listItem = {descricao, quantidade: parseInt(quantidade)};
+    Database.saveItem(listItem)
+      .then(response => navigation.navigate("AppList", listItem));
+  }
+
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Item para comprar</Text>
         <View style={styles.inputContainer}> 
           <TextInput 
             style={styles.input} 
+            onChangeText={handleDescriptionChange}
             placeholder="O que está faltando em casa?"
             clearButtonMode="always" /> 
           <TextInput 
             style={styles.input}  
+            onChangeText={handleQuantityChange}
             placeholder="Digite a quantidade" 
             keyboardType={'numeric'}
             clearButtonMode="always" /> 
-          <TouchableOpacity style={styles.button}> 
+          <TouchableOpacity style={styles.button} onPress={ handleButtonPress}> 
+          
             <Text style={styles.buttonText}>Salvar</Text> 
           </TouchableOpacity> 
         </View>
         <StatusBar style="light" />
       </View>
     );
-  }
+    }
 
   const styles = StyleSheet.create({
     container: {
